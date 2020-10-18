d217efeb024213473007963a69e800f476a0d067
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-10-18 16:13:16 +0100

    Update build scripts and enable linux build
    clean up Python 2.x remnants

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,21 +3,14 @@
 set -e
 set -x
 
-
 if [ `uname` = "Darwin" ]; then
-	FLAGS="-std=c++14"
+    FLAGS="-std=c++14"
 else
-	FLAGS="-std=c++11"
-fi
-
-if [[ $PY3K -eq 1 || $PY3K == "True" ]]; then
-  BUILD_PYTHON="-DBUILD_PYTHON_INTERFACE=ON"
-else
-  BUILD_PYTHON="-DBUILD_PYTHON2_INTERFACE=ON"
+    FLAGS="-std=c++11"
 fi
 
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=$FLAGS ${BUILD_PYTHON} -DCMAKE_INSTALL_PREFIX=$PREFIX ../
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=$FLAGS -DBUILD_PYTHON_INTERFACE=ON -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j $CPU_COUNT
 make install
 
